28d9d3d555ef21a4f77df3ccc75ecfc02be5140a
Beaglefoot <ch-e-st@yandex.ru>
2018-07-09 10:14:00 +0300

    Already voted check

--- server/src/controllers/polls/addVote.ts
+++ server/src/controllers/polls/addVote.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import Poll, { IPoll } from '../../models/Poll';
+import getIP from '../../helpers/getIP';
 
 const addVote: express.Handler = async (req, res) => {
   const { pollID } = req.params;
@@ -7,16 +8,38 @@ const addVote: express.Handler = async (req, res) => {
   let poll: IPoll;
 
   try {
+    const isVoted = Boolean(
+      await Poll.findOne(
+        Object.assign(
+          { _id: pollID },
+          req.user
+            ? { 'voters.user': req.user.id }
+            : { 'voters.ip': getIP(req) }
+        )
+      )
+    );
+
+    if (isVoted)
+      throw Object.assign(new Error('Already voted.'), { httpStatusCode: 403 });
+
     poll = await Poll.findOneAndUpdate(
       { _id: pollID, 'options.name': name },
-      { $inc: { 'options.$.votes': 1 } }
+      {
+        $inc: { 'options.$.votes': 1 },
+        $push: {
+          voters: Object.assign(
+            { ip: getIP(req) },
+            req.user ? { user: req.user.id } : {}
+          )
+        }
+      }
     );
 
     if (!poll) throw new Error('Not found.');
 
     poll = await Poll.findById(pollID);
   } catch (err) {
-    err.httpStatusCode = 404;
+    if (!err.httpStatusCode) err.httpStatusCode = 404;
     throw err;
   }
 
