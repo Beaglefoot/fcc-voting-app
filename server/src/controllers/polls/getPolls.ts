4f65845e11fa00b3901d16aa8fd5e788f503b18b
Beaglefoot <ch-e-st@yandex.ru>
2018-07-20 23:22:44 +0300

    Add route and extend getPolls controller
    Now it's possible to get list of polls for a given user.

--- server/src/controllers/polls/getPolls.ts
+++ server/src/controllers/polls/getPolls.ts
@@ -1,13 +1,38 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import User, { IUser } from '../../models/User';
 import Poll, { IPoll } from '../../models/Poll';
 
 const getPolls: express.Handler = async (req, res) => {
-  let polls: IPoll[];
+  const { userID } = req.params;
+  let polls: IPoll[] | any;
 
   try {
-    polls = await Poll.aggregate([
-      { $project: { title: true, votesCount: { $size: '$voters' } } }
-    ]);
+    polls = userID
+      ? await User.aggregate([
+          { $match: { _id: mongoose.Types.ObjectId(userID) } },
+          { $project: { polls: '$polls', _id: false } },
+          { $unwind: '$polls' },
+          {
+            $lookup: {
+              from: 'polls',
+              foreignField: '_id',
+              localField: 'polls',
+              as: 'poll'
+            }
+          },
+          { $unwind: '$poll' },
+          {
+            $project: {
+              _id: '$poll._id',
+              title: '$poll.title',
+              votesCount: { $size: '$poll.voters' }
+            }
+          }
+        ])
+      : await Poll.aggregate([
+          { $project: { title: true, votesCount: { $size: '$voters' } } }
+        ]);
   } catch (err) {
     err.httpStatusCode = 404;
     throw err;
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import asyncMiddleware from '../middleware/async';
 import getUser from '../controllers/users/getUser';
+import getPolls from '../controllers/polls/getPolls';
 
 const router = Router();
 
 router.get('/:userID', asyncMiddleware(getUser));
+router.get('/:userID/polls', asyncMiddleware(getPolls));
 
 export default router;
