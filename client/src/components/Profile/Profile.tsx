95afd9c4d0fd46d6b782a3806567246f9706be5e
Beaglefoot <ch-e-st@yandex.ru>
2018-07-21 23:56:28 +0300

    Add PollList in Profile component

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -3,15 +3,22 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
 import { IAuth } from 'src/state/state';
+import PollsList from 'src/components/PollsList/PollsList';
+import providePolls from 'src/hocs/providePolls';
 
 interface IProps {
   auth: IAuth;
 }
 
+let UserPolls: React.ComponentClass;
+
 const Profile = ({ auth: { fetchStatus, data, error } }: IProps) => {
+  if (!UserPolls && data)
+    UserPolls = providePolls(`/api/users/${data._id}/polls`)(PollsList);
+
   if (fetchStatus === 'done' && data === '') return <Redirect to="/" />;
   if (fetchStatus === 'error') return <div>{error}</div>;
-  return <div>Profile</div>;
+  return UserPolls ? <UserPolls /> : <div />;
 };
 
 export default connect(({ auth }: IProps) => ({ auth }))(Profile);
