e828ce9f66666eb41978648cca8806b217f2e0c7
Beaglefoot <ch-e-st@yandex.ru>
2018-07-26 18:11:53 +0300

    Always send ip with user data

--- server/src/controllers/users/currentUser.ts
+++ server/src/controllers/users/currentUser.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import getIP from '../../helpers/getIP';
 
 const currentUser: express.Handler = (req, res) => {
-  res.send(req.user);
+  res.send(Object.assign({ ip: getIP(req) }, req.user && req.user.toObject()));
 };
 
 export default currentUser;
