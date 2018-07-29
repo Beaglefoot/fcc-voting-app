00b36b12a5c5381d06c565b7b5e64056fb5b53ea
Beaglefoot <ch-e-st@yandex.ru>
2018-07-29 22:54:37 +0300

    Show confirmation on poll deletion

--- client/src/components/PollsList/PollsList.tsx
+++ client/src/components/PollsList/PollsList.tsx
@@ -9,6 +9,7 @@ import {
   IPollsDeletion
 } from 'src/state/state';
 import Spinner from 'src/components/Spinner/Spinner';
+import Alert from 'src/components/Alert/Alert';
 import {
   pollsList,
   listItem,
@@ -25,41 +26,87 @@ interface IProps {
   pollDeletion: IPollsDeletion;
 }
 
-const PollsList = ({
-  polls: { fetchStatus, data, error },
-  auth,
-  deletePoll,
-  pollDeletion
-}: IProps) => (
-  <ul className={pollsList}>
-    {{
-      done: () =>
-        data.map(poll => (
-          <li className={listItem} key={poll._id}>
-            <Link className={link} to={`/poll/${poll._id}`}>
-              {`${poll.title} (${poll.votesCount})`}
-            </Link>
+interface IState {
+  confirmation: {
+    isShown: boolean;
+    pollTitle?: string;
+    pollID?: string;
+  };
+}
 
-            {auth.data.polls &&
-              auth.data.polls[poll._id] &&
-              (pollDeletion[poll._id] ? (
-                <Spinner className={smallSpinner} />
-              ) : (
-                <div
-                  className={cross}
-                  onClick={() => {
-                    deletePoll(poll._id);
-                  }}
-                >
-                  ✖
-                </div>
-              ))}
-          </li>
-        )),
-      pending: () => <Spinner />,
-      error: () => <div>{error}</div>
-    }[fetchStatus]()}
-  </ul>
-);
+class PollsList extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      confirmation: {
+        isShown: false
+      }
+    };
+  }
+
+  showAlert(pollID: string, pollTitle: string) {
+    this.setState(() => ({
+      confirmation: { isShown: true, pollTitle, pollID }
+    }));
+  }
+
+  hideAlert = () => this.setState(() => ({ confirmation: { isShown: false } }));
+
+  render() {
+    const {
+      polls: { fetchStatus, data, error },
+      auth,
+      deletePoll,
+      pollDeletion
+    } = this.props;
+
+    const { confirmation } = this.state;
+
+    return (
+      <ul className={pollsList}>
+        {{
+          done: () =>
+            data.map(poll => (
+              <li className={listItem} key={poll._id}>
+                <Link className={link} to={`/poll/${poll._id}`}>
+                  {`${poll.title} (${poll.votesCount})`}
+                </Link>
+
+                {auth.data.polls &&
+                  auth.data.polls[poll._id] &&
+                  (pollDeletion[poll._id] ? (
+                    <Spinner className={smallSpinner} />
+                  ) : (
+                    <div
+                      className={cross}
+                      onClick={() => this.showAlert(poll._id, poll.title)}
+                    >
+                      ✖
+                    </div>
+                  ))}
+              </li>
+            )),
+          pending: () => <Spinner />,
+          error: () => <div>{error}</div>
+        }[fetchStatus]()}
+
+        {confirmation.isShown && (
+          <Alert
+            onOk={() => {
+              deletePoll(confirmation.pollID);
+              this.hideAlert();
+            }}
+            onCancel={this.hideAlert}
+          >
+            {`This will delete the poll named "${
+              confirmation.pollTitle
+            }". Continue?`}
+          </Alert>
+        )}
+      </ul>
+    );
+  }
+}
 
 export default connect(({ auth }: IGlobalState) => ({ auth }))(PollsList);
